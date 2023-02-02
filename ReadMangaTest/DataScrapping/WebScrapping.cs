5565a9e26bb12cbe6c53ddee49985299db5162b2
tpn <tapnguyen01@gmail.com>
2023-02-02 16:38:21 +0700

    Crape Chapter

--- ReadMangaTest/DataScrapping/WebScrapping.cs
+++ ReadMangaTest/DataScrapping/WebScrapping.cs
@@ -19,35 +19,35 @@ public class WebScrapping
         // Use HtmlWeb to download the HTML content of the web page
         var htmlWeb = new HtmlWeb();
         var htmlDocument = htmlWeb.Load(url);
-        
+
         // Use BeautifulSoup to parse the HTML content
         var htmlSoup = new HtmlAgilityPack.HtmlDocument();
         htmlSoup.LoadHtml(htmlDocument.DocumentNode.OuterHtml);
-        
+
         // Select the HTML nodes that contain the data you want to scrape
         return htmlSoup.DocumentNode.SelectNodes(xPath);
     }
+
     private HtmlNode GetHtmlNode(string url, string xPath)
     {
         // Use HtmlWeb to download the HTML content of the web page
         var htmlWeb = new HtmlWeb();
         var htmlDocument = htmlWeb.Load(url);
-        
+
         // Use BeautifulSoup to parse the HTML content
         var htmlSoup = new HtmlAgilityPack.HtmlDocument();
         htmlSoup.LoadHtml(htmlDocument.DocumentNode.OuterHtml);
-        
+
         // Select the HTML nodes that contain the data you want to scrape
         return htmlSoup.DocumentNode.SelectSingleNode(xPath);
     }
 
     public List<Category> ScrapeCategory()
     {
-        
         // URL of the web page you want to scrape data from
         var url = "https://www.nettruyenup.com";
 
-        var categoryGrandNode = GetHtmlNode(url,"//body//nav[@id='mainNav']//li[@class='dropdown']");
+        var categoryGrandNode = GetHtmlNode(url, "//body//nav[@id='mainNav']//li[@class='dropdown']");
 
         var categoryNodes = categoryGrandNode.SelectNodes(".//ul[@class='nav']//li");
         // Console.WriteLine(categoryNodes);
@@ -58,12 +58,14 @@ public class WebScrapping
             return null;
         }
 
-        
+
         foreach (var categoryNode in categoryNodes)
         {
             var detailUrls = categoryNode.SelectSingleNode(".//a").Attributes["href"].Value;
-            
-            var descriptionCategory = GetHtmlNode(detailUrls, "//main[@class='main']//div[@class='container']//div[@id='ctl00_mainContent_ctl00_divDescription']//div").InnerHtml.Trim();        
+
+            var descriptionCategory = GetHtmlNode(detailUrls,
+                    "//main[@class='main']//div[@class='container']//div[@id='ctl00_mainContent_ctl00_divDescription']//div")
+                .InnerHtml.Trim();
             var nameCategory = "";
             if (categoryNode.SelectSingleNode(".//a//strong") is not null)
             {
@@ -83,6 +85,7 @@ public class WebScrapping
             // Console.WriteLine(category.Description);
             categories.Add(category);
         }
+
         return categories;
         // return;
         _context.Categories.AddRange(categories);
@@ -95,7 +98,7 @@ public class WebScrapping
         // URL of the web page you want to scrape data from
         var url = "https://www.nettruyenup.com";
 
-        var comicNodes = GetHtmlNodes(url, 
+        var comicNodes = GetHtmlNodes(url,
             "//main[@class='main']//div[@class='container']//div[@class='row']//div[@class='row']//div[@class='item']");
         Console.WriteLine(1);
         if (comicNodes == null)
@@ -111,10 +114,10 @@ public class WebScrapping
         Console.WriteLine(3);
         var categoryList = ScrapeCategory();
 
-        
+
         var comics = new List<Comic>();
         var artists = new List<Artist>();
-        var categories = new List<Category>();
+        var chapters = new List<Chapter>();
         var comicCategories = new List<ComicCategory>();
         var nullArtist = new Artist()
         {
@@ -129,13 +132,39 @@ public class WebScrapping
             count++;
             string detailUrl = comicNode.SelectSingleNode(".//div//a").Attributes["href"].Value;
 
-            var detailComicNode = GetHtmlNode(detailUrl, 
+            var detailComicNode = GetHtmlNode(detailUrl,
                 "//main[@class='main']//div[@class='container']//div[@class='row']//div[@class='detail-info']//ul[@class='list-info']");
 
             var nameArtist = detailComicNode.SelectSingleNode(".//li[@class='author row']//p[@class='col-xs-8']//a");
-            
+
             var nameCategories = detailComicNode.SelectNodes(".//li[@class='kind row']//p[@class='col-xs-8']//a");
 
+            var nodeChapters = GetHtmlNode(detailUrl,
+                "//main[@class='main']//div[@class='container']//div[@class='row']//div[@class='list-chapter']");
+
+            List<string> chapterUrls = new List<string>();
+            // Console.WriteLine(urlChapters.InnerHtml.Trim());
+            if (nodeChapters.SelectNodes("//li[@class='row']//a") != null)
+                foreach (var urlChapter in nodeChapters.SelectNodes("//li[@class='row']//a"))
+                {
+                    var chapterUrl = urlChapter.Attributes["href"].Value;
+                    chapterUrls.Add(chapterUrl);
+                }
+
+            if (nodeChapters.SelectNodes("//li[@class='row less']//a") != null)
+                foreach (var urlChapter in nodeChapters.SelectNodes("//li[@class='row less']//a"))
+                {
+                    var chapterUrl = urlChapter.Attributes["href"].Value;
+                    chapterUrls.Add(chapterUrl);
+                }
+
+            if (chapterUrls == null)
+            {
+                Console.WriteLine("url null " + count);
+            }
+
+
+            // return;
             Artist artist;
             if (nameArtist == null)
             {
@@ -159,7 +188,7 @@ public class WebScrapping
                 Wallpaper = comicNode.SelectSingleNode(".//a//img").Attributes["data-original"].Value,
                 Artist = artist
             };
-            
+
             foreach (var nameCategory in nameCategories)
             {
                 var categoryName = nameCategory.InnerHtml.Trim();
@@ -174,16 +203,38 @@ public class WebScrapping
                         };
                         comicCategories.Add(comicCategory);
                     }
-
                 }
             }
 
+            var chapterList = new List<Chapter>();
+            foreach (var chapterUrl in chapterUrls)
+            {
+                var chapterNode = GetHtmlNode(chapterUrl,
+                    "//main[@class='main']//div[@class='container']//div[@class='reading']");
+                var nameChapter = chapterNode.SelectSingleNode(".//span[@itemprop='name']").InnerText.Trim();
+                var contentNodes = chapterNode.SelectNodes(".//div[@class='page-chapter']");
+                string contents = "";
+                foreach (var contentNode in contentNodes)
+                {
+                    contents += ","+ contentNode.SelectSingleNode(".//img").Attributes["src"].Value;
+                }
+                
+                var chapter = new Chapter()
+                {
+                    Name = nameChapter,
+                    Content = contents,
+                    Comic = comic,
+                };
+                chapterList.Add(chapter);
+            }
 
             // Console.WriteLine(comic.Name);
             // Console.WriteLine(comic.Artist.Name);
+            chapters.AddRange(chapterList);
             artists.Add(artist);
             comics.Add(comic);
         }
+
         artists = artists.Distinct().ToList();
         // Console.WriteLine("\nManga: ");
         // foreach (var comic in comics)
@@ -213,6 +264,7 @@ public class WebScrapping
         // Save the extracted data to the database
         _context.Artists.AddRange(artists);
         _context.Comics.AddRange(comics);
+        _context.Chapters.AddRange(chapters);
         _context.Categories.AddRange(categoryList);
         _context.ComicCategories.AddRange(comicCategories);
         _context.SaveChanges();
